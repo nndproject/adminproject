0ab5d378710649af2fccb32a4b8be9a46511c0d1
fernandoferry <teamdevnnd@gmail.com>
2021-09-08 00:46:29 +0700

    default files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create('id_ID');
+        $password = 'admin123';
+        // create demo users
+        $role= Role::where('name', 'client')->first(); 
+
+        for ($i=0; $i <= 50 ; $i++) { 
+
+            $user = \App\Models\User::factory()->create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'status' => 'Active',
+                'password' => $password
+            ]);
+            $user->assignRole($role);
+        }
     }
 }
